72c7e6e65dd96c12e0c720146e101dc2f2e7579e
Huffbin <spiderman-louis@outlook.com>
2026-07-19 12:02:04 +0800

    Create museum landing page design

---
+++ index.html/script.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll("button");
+
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        console.log(button.innerText + " selected");
+
+    });
+
+});
